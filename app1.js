1c83dc1730fcdb699a286c813b32a2130d181ce3
Aziz Turan <aziz.turan@albarakatech.com>
2022-06-20 14:24:48 +0300

    arguments object

--- app1.js
+++ app1.js
@@ -8,3 +8,15 @@ const makeAdder=(num1)=>{
 // and takes a parameter num2
 var adder=makeAdder(2);
 console.log(`result:${adder(3)}`);
+
+// function with unknow parameters 
+function addNumers(){
+    let sum=0;
+    let args=[...arguments];
+    console.log(args.length);
+    args.forEach(param => {
+        sum+=param;
+    });
+    return sum;
+}
+console.log(addNumers(1,2,3));
