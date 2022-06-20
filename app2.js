672e00b269519f183a38b62a78f74c4dd1cecf30
Aziz Turan <aziz.turan@albarakatech.com>
2022-06-20 15:20:00 +0300

    array tests - find at fill

--- app2.js
+++ app2.js
@@ -8,3 +8,14 @@ let arrFiltered=arr.filter(f);
 console.log(`after filter :${arrFiltered}`);
 arrFiltered=arrFiltered.map(elm=>elm+5);
 console.log(arrFiltered);
+
+// array find method
+console.log(arr.find(elm=>elm>2));
+// array at method 
+console.log(arr.at(0));
+// array fill method
+let arr1=new Array(7);
+for(var i=0;i<7;i++){
+    arr1.fill(i,i,i+1);
+}
+console.log(arr1);
